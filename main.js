ee04135a5ac5ad71cd7922c8a8008382060de864
GatoSeries <121359333+GatoSeries@users.noreply.github.com>
2023-06-03 10:45:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+Webcam.set({
+    width: 305,
+    height: 235,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take(){
+Webcam.snap(function(data_uri){
+    document.getElementById("foto").innerHTML = '<img id="capture_img" src="' + data_uri + '"/>';
+});
+}
+console.log("ml5 versiom: " + ml5.version)
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/XgqbdYweV/model.json", modelLoaded);
+
+function modelLoaded(){
+    console.log("Model loaded");
+}
